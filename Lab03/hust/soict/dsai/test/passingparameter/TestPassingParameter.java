adeb5d2535eabe1b2a50756b4cbcb61b75121a62
Sanemy <quanghuy200vhZz@gmail.com>
2026-04-24 18:13:07 +0700

    Done task 3: Test passing parameter and implement correct swap

--- Lab03/hust/soict/dsai/test/passingparameter/TestPassingParameter.java
+++ Lab03/hust/soict/dsai/test/passingparameter/TestPassingParameter.java
@@ -1,21 +1,31 @@
-package hust.soict.dsai.test.passingparameter; 
+package hust.soict.dsai.test.passingparameter;
+
 import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
 public class TestPassingParameter {
+
     public static void main(String[] args) {
         DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
         DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
 
-        // Gọi hàm swap mới
         swap(jungleDVD, cinderellaDVD);
-        
-        System.out.println("After swap - jungle dvd title: " + jungleDVD.getTitle());
-        System.out.println("After swap - cinderella dvd title: " + cinderellaDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
     }
 
-    public static void swap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
-        String tmpTitle = dvd1.getTitle();
-        dvd1.setTitle(dvd2.getTitle());
-        dvd2.setTitle(tmpTitle);
 
+    public static void swap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        String oldTitle = dvd1.getTitle();
+        dvd1.setTitle(dvd2.getTitle());
+        dvd2.setTitle(oldTitle);
+    }
+
+    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+        String oldTitle = dvd.getTitle();
+        dvd.setTitle(title);
+        dvd = new DigitalVideoDisc(oldTitle); 
     }
 }
