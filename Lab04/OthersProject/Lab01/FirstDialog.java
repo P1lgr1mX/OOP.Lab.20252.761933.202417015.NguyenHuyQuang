841d0f517c7d6a8372609fb8c8f4921cee31c793
P1lgr1m <P1lgr1m@users.noreply.github.com>
2026-05-22 18:28:24 +0700

    Fix structure

---
+++ Lab04/OthersProject/Lab01/FirstDialog.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane ;
+class FirstDialog{
+    public static void main(String[] args){
+	JOptionPane.showMessageDialog(null , "Welcome to my world") ;
+	System.exit(0) ;
+    }
+}
